cfe7c66ab1f8933a93c477b7b5248482d436e776
Swarup-py <swarup3125@gmail.com>
2024-09-22 22:21:17 +0530

    Added Basic React Layout

---
+++ src/pages/Setting.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+
+function Setting() {
+    return(
+        <>
+            <Navbar current={"Setting"}/>
+            <h1>TODO</h1>
+        </>
+    )
+}
+
+export default Setting;
